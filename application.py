a97b570181c927667c66887296e84e998c2e2f9c
Rihan-J <rihanj27.pvt@gmail.com>
2026-08-27 11:27:58 +0530

    Initial commit

--- application.py
+++ application.py
@@ -42,7 +42,7 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
-
+if __name__ == "__main__":
+    print("\n🚀 Home URL: http://127.0.0.1:5000/")
+    print("🎯 Prediction URL: http://127.0.0.1:5000/predictdata\n")
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
